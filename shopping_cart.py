9633655b55cb141980ca20dffb1cd1a3df853160
Alex Stein <alex22bs@gmail.com>
2021-03-12 10:43:11 -0300

    improved the list to display aligned and other bugs

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,7 +1,7 @@
-#create the essencial lists to be used
+#create the essencial lists and variable to be used
 items = []
 prices = []
-
+bigger = ""
 #possible actions presentation
 print()
 print("Welcome to the shopping cart program!")
@@ -12,13 +12,13 @@ print("2.View cart")
 print("3.Remove item")
 print("4.Compute total")
 print("5.Quit")
-action = int(input("Please enter an action number: "))
+action = str(input("Please enter an action number: "))
 
-while action != 5:
+while action != "5":
     #add item
-    if action == 1:
+    if action == "1":
         add = "yes"
-        while add == "yes":
+        while add == "yes" or add == "y":
             item = str(input("What item would you like to add? "))
             items.append(item)
             price = float(input(f"What is the price of the '{item}'? "))
@@ -28,23 +28,31 @@ while action != 5:
             print()
 
     #view cart
-    if action == 2:
+    if action == "2":
         count = -1
         print()
         if items:
             print("The content of the shopping cart are: ")
             for item in items:
+                if len(item) > len(bigger):
+                    bigger = item
+            for item in items:
                 count = count + 1
                 price = "{:.2f}".format(prices[count])
-                print(f"{(count+1):>0}. {items[count].capitalize():>2} {'-  $':>8}  {price:>1}")
+                show_item = items[count].capitalize()
+                i = len(bigger) 
+                index = str(count+1)+"."
+                show_price = "$"+str(price)
+                #print(f"{count+1}. {show_item:<5s} - {price:>1}")
+                print(f"{index} {show_item:{i}}  -  {show_price:{i}}")
         else:
             print("The cart is empty")
         print()
 
     #remove item
-    if action == 3:
+    if action == "3":
         delete_more = "yes"
-        while delete_more.lower() == "yes":
+        while delete_more.lower() == "yes" or delete_more.lower() == "y":
             item_to_del = int(input("What item number do you want to remove? ")) -1
             if len(items) > item_to_del and item_to_del > -1:
                 items.pop(item_to_del)
@@ -59,7 +67,7 @@ while action != 5:
         print()
 
     #compute total
-    if action == 4:
+    if action == "4":
         print()
         summ = 0
         for price in prices:
@@ -69,7 +77,7 @@ while action != 5:
         print()
 
     #in the case you select a not possible action
-    if action > 5 or action < 1:
+    if action != "1" and action != "2" and action != "3" and action != "4" and action != "5":
         print()
         print("Not possible action")
         print()
@@ -81,7 +89,7 @@ while action != 5:
     print("3.Remove item")
     print("4.Compute total")
     print("5.Quit")
-    action = int(input("Please enter an action: "))
+    action = str(input("Please enter an action: "))
 
     
 
